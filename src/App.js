1e8ceae8f81978e8e02d8ab4d63339b6b399de9c
Gustavo Henrique <avoguga@gmail.com>
2021-03-12 23:05:34 -0300

    sidebar-item feito e css att

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Style.css';
 import Slider from './Slider';
+import SidebarItem from './SidebarItem';
 function App() {
   return (
     <div className="container">
-      <div className="main-image">image</div>
+      <div className="main-image"/>
       <div className="sidebar">
-      sidebar
+        <SidebarItem/>
+        <SidebarItem/>
       </div>
       <Slider/>
     </div>
